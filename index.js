5954483d9b532f76ccf15235c0c46998f45187ce
najmul905 <nh4808563@gmail.com>
2023-05-03 08:01:35 +0600

    load data from chef details json file to index.js

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express')
+const chefs=require('./ChefDetails.json')
+const cors=require('cors')
 const app = express()
 const port = 3000
 
+app.use(cors())
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('hello world')
+});
+
+app.get('/chefs',(req,res)=>{
+    res.send(chefs)
 })
+// app.get('/chefs/:id'(req,res)=>{
+//     const id=req.perems.id
+//     console.log('i got the id number',id)
+// })
 
 app.listen(port, () => {
   console.log(`Famous chefs dish API load ${port}`)
